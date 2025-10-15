a8d7b04e702b0cc9a793f5d023059e44e6371eec
Vikram Hem Chandar <vikramhemchandar@gmail.com>
2025-10-15 20:47:12 +0530

    feature/circle-area git stash has been implement

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -6,9 +6,9 @@ class GeometryCalculator:
         return length * width
 if __name__ == "__main__":
     calculator = GeometryCalculator()
-    # TODO: Implement the feature to calculate the area of a circle
-    # radius = 5
-    # print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+    #Implement the feature to calculate the area of a circle
+    radius = 5
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
     #Implement the feature to calculate the area of a rectangle 
     #length = 10
     #width = 6
